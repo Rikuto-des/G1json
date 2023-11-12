d81b2301f501ec58d35923a21f4dfa89d33ca0b2
RikutoYasuda <ricton513@gmail.com>
2023-11-12 18:23:37 +0900

    Initial Commit

---
+++ G1json/G1jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  G1jsonApp.swift
+//  G1json
+//
+//  Created by user on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct G1jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
